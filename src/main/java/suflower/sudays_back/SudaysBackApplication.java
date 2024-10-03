68d5ccc711f5050ca20bec2127fd8786a018f4b4
alscjf1329 <alscjf1329@gmail.com>
2024-10-03 20:10:34 +0900

    feat: Create SUDAYS_back project

---
+++ src/main/java/suflower/sudays_back/SudaysBackApplication.java
@@ -0,0 +1,13 @@
+package suflower.sudays_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudaysBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SudaysBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/suflower/sudays_back/SudaysBackApplicationTests.java
@@ -0,0 +1,13 @@
+package suflower.sudays_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudaysBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
